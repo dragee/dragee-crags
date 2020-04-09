9d21e8d84365025a78275ca0f3a853dcd26699d0
Volodymyr Myskov <jaromudr@gmail.com>
2020-04-09 12:44:35 +0300

    First Version of editor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Crag />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Crag from './components/Crag.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Crag
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
 }
 </style>
